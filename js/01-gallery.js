bbc623c64a5574461054dede202bce2acf827bd1
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-02-08 21:48:28 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,14 +2,32 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const gallery = document.querySelector('.gallery');
 const modal = document.querySelector('.lightbox');
-const modalImg = document.querySelector('');
+const modalImg = document.querySelector('.lightbox_img');
 
-function createGalleryMarkup({ preview, original, description }) {
-  <div class="gallery__item">
+const galleryMarkup = galleryItems.map(({ preview, original, description }) => {
+  return ` <div class="gallery__item">
     <a class="gallery__link" href="${original}">
       <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
     </a>
-  </div>;
-}
+  </div>`;
+});
 
-console.log(createGalleryItems(galleryItems));
+gallery.insertAdjacentHTML('beforeend', galleryMarkup.join(''));
+
+gallery.addEventListener('click', event => {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  modal.classList.add('is-open');
+  modalImg.src = event.target.dataset.source;
+  modalImg.alt = event.target.alt;
+});
+
+modal.addEventListener('click', event => {
+  if (event.target.nodeName !== 'IMG') {
+    modal.classList.remove('is-open');
+    modalImg.src = '';
+    modalImg.alt = '';
+  }
+});
