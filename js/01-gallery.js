e0dc521dc7a9b0e8c0cfbec393bedbec8c293c16
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-02-08 21:17:55 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const gallery = document.querySelector(".gallery");
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('.lightbox');
+const modalImg = document.querySelector('');
 
-console.log(galleryItems);
+function createGalleryMarkup({ preview, original, description }) {
+  <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+    </a>
+  </div>;
+}
 
+console.log(createGalleryItems(galleryItems));
