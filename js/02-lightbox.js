6cfa0f6b1c2b8817a6dbd38961553cab37c41905
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-02-08 21:58:46 +0200

    add ligthbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems.map(({ preview, original, description }) => {
+  return ` <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+    </a>
+  </div>`;
+});
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup.join(''));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
